db2288e068682bcc6fee1555e77370fc4d8438c3
Moses Palmér <mosespalmer@gmail.com>
2015-06-13 00:11:36 +0200

    Added factory method to generate mock traceback from Jasmine stack

--- lib/unittest_jasmine/_tb.py
+++ lib/unittest_jasmine/_tb.py
@@ -57,3 +57,60 @@ class Traceback(object):
     def tb_next(self):
         if len(self._frames) > 1:
             return Traceback(self._frames[1:], self._line_nums[1:])
+
+    @classmethod
+    def _frames(self, stack):
+        """Yields all frames and line numbers from a *Jasmine* stack as the
+        tuple ``(frame, line)``.
+
+        :param str stack: The stack text.
+        """
+        for m in self.STACK_RE.finditer(stack):
+            co_name, co_filename, line = m.group(1), m.group(2), m.group(3)
+
+            yield (Frame(Code(co_filename, co_name)), int(line))
+
+    @classmethod
+    def _is_user_test(self, frame):
+        """Determines whether a stack frame is from inside of a user test case.
+
+        :param Frame frame: The frame to check.
+
+        :return: whether the frame is from a user test case
+        """
+        # TODO: Determine this in a better way
+        return 'jasmine-core' not in frame.f_code.co_filename
+
+    @classmethod
+    def from_stack(self, stack):
+        """Generates a mock traceback from a *Jasmine* stack trace string.
+
+        This method takes a string on the form:
+
+            Error: Expected 2 to equal 1.
+                at stack (.../jasmine-core/jasmine.js:1482:17)
+                at buildExpectationResult (.../jasmine-core/jasmine.js:1452:14)
+                at Expectation.toEqual (.../jasmine-core/jasmine.js:1406:12)
+                at func (.../test.js:5:23)
+                at Object.<anonymous> (.../test.js:7:9)
+                at attemptSync (.../jasmine-core/jasmine.js:1789:24)
+                at QueueRunner.run (.../jasmine.js:1777:9)
+
+        and return an object which, when passed to :func:`traceback.print_tb`,
+        will be represented as:
+
+            File ".../test-runner.js", line 7, in Object.<anonymous>
+                func();
+            File ".../test-runner.js", line 5, in func
+                expect(2).toEqual(1);
+
+        :param str stack: The *Jasmine* stack description string.
+
+        :return: a mock
+        """
+        return self(*zip(*reversed(list(
+            itertools.takewhile(
+                lambda fl: self._is_user_test(fl[0]),
+                itertools.dropwhile(
+                    lambda fl: not self._is_user_test(fl[0]),
+                    self._frames(stack)))))))
